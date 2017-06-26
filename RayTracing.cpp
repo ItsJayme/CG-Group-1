63727a92a3efaf0c16c98c4d232ea5e7b24f2997
sajanki <s.a.janki@student.tudelft.nl>
2017-06-26 18:46:25 +0200

    Add the latest code

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -48,15 +48,15 @@ void getBoxesWithoutChildren(std::list<Box*>& result, Box* box) {
 void calculateMainBox(Box* box)
 {
 	float Xmax = -HUGE_VALF;
-    float Xmin = HUGE_VALF;
-    float Ymax = -HUGE_VALF;
-    float Ymin = HUGE_VALF;
-    float Zmax = -HUGE_VALF;
-    float Zmin = HUGE_VALF;
-	
-    for (unsigned int i = 0; i < MyMesh.triangles.size(); i++) {
-		
-        Triangle* currentTriangle = &(MyMesh.triangles[i]);
+	float Xmin = HUGE_VALF;
+	float Ymax = -HUGE_VALF;
+	float Ymin = HUGE_VALF;
+	float Zmax = -HUGE_VALF;
+	float Zmin = HUGE_VALF;
+
+	for (unsigned int i = 0; i < MyMesh.triangles.size(); i++) {
+
+		Triangle* currentTriangle = &(MyMesh.triangles[i]);
 		Vec3Df v0 = MyMesh.vertices[currentTriangle->v[0]].p;
 		Vec3Df v1 = MyMesh.vertices[currentTriangle->v[1]].p;
 		Vec3Df v2 = MyMesh.vertices[currentTriangle->v[2]].p;
@@ -115,7 +115,7 @@ void calculateMainBox(Box* box)
 		if (v2[2] < Zmin) {
 			Zmin = v2[2];
 		}
-		
+
 		box->setMaxX(Xmax);
 		box->setMinX(Xmin);
 		box->setMaxY(Ymax);
@@ -124,22 +124,23 @@ void calculateMainBox(Box* box)
 		box->setMinZ(Zmin);
 	}
 }
-	
 
+Box* BossBox = new Box();
+std::list<Box*> resultList;
 //Calculates the number of trinagles in a box, and the triangles in a box
 int TrianglesInBox(Box* box) {
 	int count = 0;
 	box->removeTriangles();
-    
+
 	for (unsigned int i = 0; i < MyMesh.triangles.size(); i++) {
 		bool b0 = false; bool b1 = false; bool b2 = false;
-        
+
 		Triangle* currentTriangle = &MyMesh.triangles[i];
 		Vec3Df v0 = MyMesh.vertices[currentTriangle->v[0]].p;
 		Vec3Df v1 = MyMesh.vertices[currentTriangle->v[1]].p;
 		Vec3Df v2 = MyMesh.vertices[currentTriangle->v[2]].p;
-		
-        if (box->getMinX() < v0[0] && box->getMaxX() > v0[0]) {
+
+		if (box->getMinX() < v0[0] && box->getMaxX() > v0[0]) {
 			if (box->getMinY() < v0[1] && box->getMaxY() > v0[1]) {
 				if (box->getMinZ() < v0[2] && box->getMaxZ() > v0[2]) {
 					box->addTriangle(currentTriangle);
@@ -161,51 +162,67 @@ int TrianglesInBox(Box* box) {
 			}
 		}
 	}
-	
+
 	return box->getNoTriangles();
 }
 
 //Splits up the box to small boxes with a maximum of maxNoTriangle triangles
 void splitBox(Box* parentBox, int maxNoTriangle) {
-    if (parentBox) {
-        int t = TrianglesInBox(parentBox);
+	if (parentBox) {
+		int t = TrianglesInBox(parentBox);
 		std::cout << "PARENT: Xmax: " << parentBox->getMaxX() << " Xmin: " << parentBox->getMinX() << std::endl;
 		std::cout << " Triangles: " << parentBox->getNoTriangles() << std::endl;
-        
-        if (t < maxNoTriangle) {
-            std::cout << "no split required" << std::endl;
-            return;
-        }
-        
-        Box* b;
-        Box* c;
-        Box* d;
-        Box* e;
-        
-        Box* ChildBoxA = new Box(); 
-        Box* ChildBoxB = new Box();
-        
-        float xlength = parentBox->getMaxX() - parentBox->getMinX();
-        ChildBoxA->setMaxX((parentBox->getMaxX() + parentBox->getMinX())/2);
-        ChildBoxA->setMinX(parentBox->getMinX());
-        ChildBoxA->setMaxY(parentBox->getMaxY());
-        ChildBoxA->setMinY(parentBox->getMinY());
-        ChildBoxA->setMaxZ(parentBox->getMaxZ());
-        ChildBoxA->setMinZ(parentBox->getMinZ());
 
-        ChildBoxB->setMaxX(parentBox->getMaxX());
-        ChildBoxB->setMinX((parentBox->getMaxX() + parentBox->getMinX()) / 2);
-        ChildBoxB->setMaxY(parentBox->getMaxY());
-        ChildBoxB->setMinY(parentBox->getMinY());
-        ChildBoxB->setMaxZ(parentBox->getMaxZ());
-        ChildBoxB->setMinZ(parentBox->getMinZ());
-        
-        parentBox->setLeftChild(ChildBoxA);
-        parentBox->setRightChild(ChildBoxB);
-        
-        splitBox(ChildBoxA,  maxNoTriangle);
-        splitBox(ChildBoxB,  maxNoTriangle);
-    }
+		if (t < maxNoTriangle) {
+			std::cout << "no split required" << std::endl;
+			return;
+		}
+
+		Box* b;
+		Box* c;
+		Box* d;
+		Box* e;
+
+		Box* ChildBoxA = new Box();
+		Box* ChildBoxB = new Box();
+
+		float xlength = parentBox->getMaxX() - parentBox->getMinX();
+		float ylength = parentBox->getMaxY() - parentBox->getMinY();
+		float zlength = parentBox->getMaxZ() - parentBox->getMinZ();
+		ChildBoxA->setMaxX(parentBox->getMaxX());
+		ChildBoxA->setMinX(parentBox->getMinX());
+		ChildBoxA->setMaxY(parentBox->getMaxY());
+		ChildBoxA->setMinY(parentBox->getMinY());
+		ChildBoxA->setMaxZ(parentBox->getMaxZ());
+		ChildBoxA->setMinZ(parentBox->getMinZ());
+
+		ChildBoxB->setMaxX(parentBox->getMaxX());
+		ChildBoxB->setMinX(parentBox->getMinX());
+		ChildBoxB->setMaxY(parentBox->getMaxY());
+		ChildBoxB->setMinY(parentBox->getMinY());
+		ChildBoxB->setMaxZ(parentBox->getMaxZ());
+		ChildBoxB->setMinZ(parentBox->getMinZ());
+		
+		if (xlength >= ylength && xlength >= zlength) {
+			ChildBoxA->setMaxX((parentBox->getMaxX() + parentBox->getMinX()) / 2);
+			ChildBoxB->setMinX((parentBox->getMaxX() + parentBox->getMinX()) / 2);
+		}
+
+		else if (ylength >= zlength && ylength >= xlength) {
+			ChildBoxA->setMaxY((parentBox->getMaxY() + parentBox->getMinY()) / 2);
+			ChildBoxB->setMinY((parentBox->getMaxY() + parentBox->getMinY()) / 2);
+		}
+		else {
+			ChildBoxA->setMaxZ((parentBox->getMaxZ() + parentBox->getMinZ()) / 2);
+			ChildBoxB->setMinZ((parentBox->getMaxZ() + parentBox->getMinZ()) / 2);
+		}
+
+		parentBox->setLeftChild(ChildBoxA);
+		parentBox->setRightChild(ChildBoxB);
+
+		splitBox(ChildBoxA, maxNoTriangle);
+		splitBox(ChildBoxB, maxNoTriangle);
+	}
 }
 
 //Bool ray intersect box
@@ -220,7 +237,7 @@ bool intersectBox(const Vec3Df &origin, const Vec3Df &direction, Box* mainbox)
 
 	//t = (xmin - Ox) / dxmin
 	float txmin = (Xmin - origin[0]) / direction[0];
-	
+
 	float txmax = (Xmax - origin[0]) / direction[0];
 
 	float tymin = (Ymin - origin[1]) / direction[1];
@@ -238,7 +255,7 @@ bool intersectBox(const Vec3Df &origin, const Vec3Df &direction, Box* mainbox)
 	float tinz = fmin(tzmin, tzmax);
 	float toutz = fmax(tzmin, tzmax);
 
-	float tin = fmax(fmax(tinx, tiny),fmax(tiny, tinz));
+	float tin = fmax(fmax(tinx, tiny), fmax(tiny, tinz));
 	float tout = fmin(fmin(toutx, touty), fmin(touty, toutz));
 
 	if (tin > tout) {
@@ -268,23 +285,22 @@ void init()
 	//here, we set it to the current location of the camera
 	MyLightPositions.push_back(MyCameraPosition);
 
-	MyMesh.loadMesh("teapot.obj", true);
+	MyMesh.loadMesh("dodgeColorTest.obj", true);
 	Kd.resize(MyMesh.vertices.size(), Vec3Df(0.5, 0.5, 0.5));
-    std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
-	
-    Ks.resize(MyMesh.vertices.size(), Vec3Df(0.5, 0.5, 0.5));
-    std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
-	
-    Shininess.resize(MyMesh.vertices.size(), 3);
-    std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
+	std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
 
-	Box* mainbox = new Box();
-    calculateMainBox(mainbox);
+	Ks.resize(MyMesh.vertices.size(), Vec3Df(0.5, 0.5, 0.5));
+	std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
+
+	Shininess.resize(MyMesh.vertices.size(), 3);
+	std::cout << "MyMesh.vertices.size(): " << MyMesh.vertices.size() << std::endl;
+
+	calculateMainBox(BossBox);
 	Box* a;
 	Box* b;
 	int maxTrianglesInBox = 1000;
-	std::cout << "init: " << TrianglesInBox(mainbox);
-	splitBox(mainbox,  maxTrianglesInBox);
+	splitBox(BossBox, maxTrianglesInBox);
+	getBoxesWithoutChildren(resultList, BossBox);
 }
 
 //Bool ray intersect plane
@@ -292,7 +308,7 @@ bool intersectPlane(const Vec3Df &normal, const Vec3Df &direction, const Vec3Df 
 {
 	// t = (dist - dot(orig, normal) / dot(direction, normal)
 	float denominator = (distance - Vec3Df::dotProduct(origin, normal));
-	if (Vec3Df::dotProduct(direction,normal) != 0) {
+	if (Vec3Df::dotProduct(direction, normal) != 0) {
 		float numerator = Vec3Df::dotProduct(direction, normal);
 		t = denominator / numerator;
 		planepos = origin + t * direction;
@@ -342,38 +358,38 @@ Vec3Df getTriangleCenter(const Vec3Df &edge1, const Vec3Df &edge2, const Vec3Df 
 //Main raytracer
 Vec3Df performRayTracing(const Vec3Df & origin, const Vec3Df & dest)
 {
-	int maxTrianglesInBox = 1;
+
 	Vec3Df direction = dest - origin;
-	Box* mainbox = new Box();
-    calculateMainBox(mainbox);
-	if (intersectBox(origin, direction, mainbox)) {
+	Box* box = BossBox->getLeftChild();	
+	std::cout << "MaxX" << box->getMaxX();
+	if (intersectBox(origin, direction, BossBox)) {
 		std::cout << "Box hit!";
-			for (unsigned int i = 0; i < MyMesh.triangles.size(); i++) {
-				Triangle currenttriangle = MyMesh.triangles[i];
+		for (unsigned int i = 0; i < MyMesh.triangles.size(); i++) {
+			Triangle currenttriangle = MyMesh.triangles[i];
 
-				Vec3Df v0 = MyMesh.vertices[currenttriangle.v[0]].p;
-				Vec3Df v1 = MyMesh.vertices[currenttriangle.v[1]].p;
-				Vec3Df v2 = MyMesh.vertices[currenttriangle.v[2]].p;
+			Vec3Df v0 = MyMesh.vertices[currenttriangle.v[0]].p;
+			Vec3Df v1 = MyMesh.vertices[currenttriangle.v[1]].p;
+			Vec3Df v2 = MyMesh.vertices[currenttriangle.v[2]].p;
 
-				Vec3Df edge12 = v0 - v1;
-				Vec3Df edge13 = v0 - v2;
-				Vec3Df normal = Vec3Df::crossProduct(edge12, edge13);
-				normal.normalize();
+			Vec3Df edge12 = v0 - v1;
+			Vec3Df edge13 = v0 - v2;
+			Vec3Df normal = Vec3Df::crossProduct(edge12, edge13);
+			normal.normalize();
 
-				float distance = Vec3Df::dotProduct(normal, v0);
-				Vec3Df planepos;
-				float t;
+			float distance = Vec3Df::dotProduct(normal, v0);
+			Vec3Df planepos;
+			float t;
 
-				if (intersectPlane(normal, direction, origin, distance, t, planepos)) {
-					std::cout << "Plane hit!";
+			if (intersectPlane(normal, direction, origin, distance, t, planepos)) {
+				std::cout << "Plane hit!";
 
-					Vec3Df trianglepos;
-					if (rayTriangleIntersect(planepos, currenttriangle, trianglepos, normal)) {
-						std::cout << "Triangle hit!";
-					}
+				Vec3Df trianglepos;
+				if (rayTriangleIntersect(planepos, currenttriangle, trianglepos, normal)) {
+					std::cout << "Triangle hit!";
 				}
-
 			}
+
+		}
 	}
 
 	return Vec3Df(dest[0], dest[1], dest[2]);
@@ -483,25 +499,25 @@ void yourDebugDraw()
 	//let's draw the lights in the scene as points
 	glPushAttrib(GL_ALL_ATTRIB_BITS); //store all GL attributes
 	glDisable(GL_LIGHTING);
-	glColor3f(1,1,1);
+	glColor3f(1, 1, 1);
 	glPointSize(10);
 	glBegin(GL_POINTS);
-	for (int i=0;i<MyLightPositions.size();++i)
+	for (int i = 0; i<MyLightPositions.size(); ++i)
 		glVertex3fv(MyLightPositions[i].pointer());
 	glEnd();
 	glPopAttrib();//restore all GL attributes
-	
-    //The Attrib commands maintain the state. 
-	//e.g., even though inside the two calls, we set
-	//the color to white, it will be reset to the previous 
-	//state after the pop.
-	//as an example: we draw the test ray, which is set by the keyboard function
+
+				  //The Attrib commands maintain the state. 
+				  //e.g., even though inside the two calls, we set
+				  //the color to white, it will be reset to the previous 
+				  //state after the pop.
+				  //as an example: we draw the test ray, which is set by the keyboard function
 	glPushAttrib(GL_ALL_ATTRIB_BITS);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_LINES);
-	glColor3f(0,1,1);
+	glColor3f(0, 1, 1);
 	glVertex3f(testRayOrigin[0], testRayOrigin[1], testRayOrigin[2]);
-	glColor3f(0,0,1);
+	glColor3f(0, 0, 1);
 	glVertex3f(testRayDestination[0], testRayDestination[1], testRayDestination[2]);
 	glEnd();
 	glPointSize(10);
@@ -511,14 +527,7 @@ void yourDebugDraw()
 	glPopAttrib();
 
 	//Draw mainBox
-	std::list<Box*> resultList;
-	Box* mainbox = new Box();
-    calculateMainBox(mainbox);
-	getBoxesWithoutChildren(resultList, mainbox);
 
-    std::cout << resultList.size();
-	
-    drawBox(mainbox);
 	for (std::list<Box*>::const_iterator iter = resultList.begin(); iter != resultList.end(); iter++) {
 		Box* box = *iter;
 		drawBox(box);
@@ -558,14 +567,14 @@ void yourKeyboardFunc(char t, int x, int y, const Vec3Df & rayOrigin, const Vec3
 	//here, as an example, I use the ray to fill in the values for my upper global ray variable
 	//I use these variables in the debugDraw function to draw the corresponding ray.
 	//try it: Press a key, move the camera, see the ray that was launched as a line.
-	testRayOrigin=rayOrigin;	
-	testRayDestination=rayDestination;
+	testRayOrigin = rayOrigin;
+	testRayDestination = rayDestination;
 	performRayTracing(testRayOrigin, testRayDestination);
 	// do here, whatever you want with the keyboard input t.
-	
+
 	//...
-	
-	std::cout<<t<<" pressed! The mouse was in location "<<x<<","<<y<<"!"<<std::endl;	
+
+	std::cout << t << " pressed! The mouse was in location " << x << "," << y << "!" << std::endl;
 }
 
 
@@ -590,7 +599,7 @@ Vec3Df phongSpecular(const Vec3Df & vertexPos, Vec3Df & normal, const Vec3Df & l
 
 Vec3Df phongModel(const Vec3Df & vertexPos, Vec3Df & normal, const Vec3Df & lightPos, const Vec3Df & cameraPos, unsigned int index)
 {
-	Vec3Df diff = phongDiffuse(vertexPos, normal, lightPos,index);
+	Vec3Df diff = phongDiffuse(vertexPos, normal, lightPos, index);
 	Vec3Df spec = phongSpecular(vertexPos, normal, lightPos, cameraPos, index);
 	return diff + spec;
 }
